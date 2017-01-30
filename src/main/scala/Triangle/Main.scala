13b08716b35bf0c37c860371ca44d35cc2c8b203
katokonn <s.kato@sonicmoov.com>
2017-01-30 14:56:28 +0900

    test

---
+++ src/main/scala/Triangle/Main.scala
@@ -0,0 +1,12 @@
+package Triangle
+
+/**
+  * Created by s.kato on 2017/01/30.
+  */
+object Main {
+  def main(args: Array[String]): Unit = {
+    println("test")
+    val Triangle = new Triangle();
+    Triangle.sample;
+  }
+}
--- src/main/scala/Triangle/Triangle.scala
+++ src/main/scala/Triangle/Triangle.scala
@@ -4,5 +4,7 @@ package Triangle
   * Created by s.kato on 2017/01/30.
   */
 class Triangle {
-  
+  def sample: Unit ={
+    println("Triangle test")
+  }
 }
